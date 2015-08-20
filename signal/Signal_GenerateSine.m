f1994dd1efa9a1b4db74db3654e93c8849ea86e6
uroboro <uroboro845@gmail.com>
2015-08-19 21:20:06 -0300

    [Signal] Revamped sine generator
    * Warning for low sample rate.
    * Error for sample rate lower than Niquist.
    * Usage of sample generator.
    * Optional phase and offset arguments.

--- signal/Signal_GenerateSine.m
+++ signal/Signal_GenerateSine.m
@@ -1,7 +1,22 @@
 % Generate sine wave
-function [ out, time ] = Signal_GenerateSine( amplitude, frequency, phase, offset, samples, samplingFrequency )
-    time = 0:1/samplingFrequency:(samples - 1) / samplingFrequency;
-%    time = linspace(0, (samples - 1) / samplingFrequency, samplingFrequency);
-    time = time.';
-    out = offset + amplitude * sin(2 * pi * frequency * time + phase);
+function [ data, time ] = Signal_GenerateSine( samples, samplingFrequency, amplitude, frequency, varargin )
+	defaults = [0, 0];
+	for i = 1:length(varargin)
+		% copio los argumentos que existen.
+		defaults(i) = varargin{i};
+	end
+	phase = defaults(1);
+	offset = defaults(2);
+
+	if (samplingFrequency/2 <= frequency)
+		error('Sampling error: (50%) sampling frequency cannot be lower than signal frequency');
+	end
+	if (samplingFrequency <= 8000)
+		warning('Sampling warning: sampling frequency will be upped to telephone sampling frequency (8kHz). Samples will be proportionally be increased.');
+		samples = samples * 8000 / samplingFrequency;
+		samplingFrequency = 8000;
+	end
+
+	time = Base_GenerateSamples(samples, samplingFrequency);
+    data = offset + amplitude * sin(2 * pi * frequency * time + phase);
 end
