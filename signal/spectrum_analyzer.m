e1bb4e7365ad7d3b93942c54c357640963cba81d
uroboro <uroboro845@gmail.com>
2015-08-19 23:21:01 -0300

    [Signal] Spectrum analyzer works
    * Switched to switch statement for data creation: 0 for file source, 1
    for telephony generator, 2 for arbitrary sine generator.
    * Plots signal and creates spectrum image from FFT array.

--- signal/spectrum_analyzer.m
+++ signal/spectrum_analyzer.m
@@ -1,38 +1,49 @@
 % Get data
-read = 0;
-if (read == 1)
+switch (1)
+case 0
 	[file, data, freq] = Audio_Load('imaginary_mp3cut.wav');
 	Audio_Play(file, data, freq);
-	dataLeft = data(:, 1);
-else
-    freq = 44100;
-	T = 441;
-	dataLeft = [];
-    T_ = 0:1/freq:((T-1)/freq);
-    dataLeft = [dataLeft (0.6*sin(2*pi* 100 * T_) + 0.3*sin(2*pi* 500 * T_))];
-    dataLeft = [dataLeft (0.6*sin(2*pi* 200 * T_) + 0.3*sin(2*pi* 600 * T_))];
-    dataLeft = [dataLeft (0.6*sin(2*pi* 300 * T_) + 0.3*sin(2*pi* 700 * T_))];
-    dataLeft = [dataLeft (0.6*sin(2*pi* 400 * T_) + 0.3*sin(2*pi* 800 * T_))];
-
-%     [s1, ~] = Signal_GenerateSine(0.6, 200, 0, 0, T, freq);
-%     [s2, ~] = Signal_GenerateSine(0.3, 500, 0, 0, T, freq);
-%  	dataLeft = [dataLeft s1+s2];
-%     [s1, ~] = Signal_GenerateSine(0.6, 300, 0, 0, T, freq);
-%     [s2, ~] = Signal_GenerateSine(0.3, 450, 0, 0, T, freq);
-%  	dataLeft = [dataLeft s1+s2];
-%     [s1, ~] = Signal_GenerateSine(0.6, 150, 0, 0, T, freq);
-%     [s2, ~] = Signal_GenerateSine(0.3, 400, 0, 0, T, freq);
-%  	dataLeft = [dataLeft s1+s2];
-%     [s1, ~] = Signal_GenerateSine(0.6, 250, 0, 0, T, freq);
-%     [s2, ~] = Signal_GenerateSine(0.3, 850, 0, 0, T, freq);
-%  	dataLeft = [dataLeft s1+s2];
-	%sound(dataLeft, freq);
+	data_ = data(:, 1);
+case 1
+	freq = 44100;
+	T = 441*50;
+	data_ = [];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '1')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '5')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '9')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '2')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '4')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '6')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '8')];
+	data_ = [data_; Telephony_PhoneTone(T, freq, '0')];
+	sound(data_, freq);
+case 2
+	freq = 44100;
+	T = 441*50;
+	m = 10;
+	data_ = [];
+	[s1, ~] = Signal_GenerateSine(T, freq, 0.6, 10*m);
+	[s2, ~] = Signal_GenerateSine(T, freq, 0.3, 50*m);
+ 	data_ = [data_; s1+s2];
+	[s1, ~] = Signal_GenerateSine(T, freq, 0.6, 20*m);
+	[s2, ~] = Signal_GenerateSine(T, freq, 0.3, 60*m);
+ 	data_ = [data_; s1+s2];
+	[s1, ~] = Signal_GenerateSine(T, freq, 0.6, 30*m);
+	[s2, ~] = Signal_GenerateSine(T, freq, 0.3, 70*m);
+ 	data_ = [data_; s1+s2];
+	[s1, ~] = Signal_GenerateSine(T, freq, 0.6, 40*m);
+	[s2, ~] = Signal_GenerateSine(T, freq, 0.3, 50*m);
+ 	data_ = [data_; s1+s2];
+	sound(data_, freq);
 end
-figure(1)
-subplot(2, 1, 1);
-plot(1:length(dataLeft), dataLeft);
-title('Signal');
-xlabel('Time (s)');
-ylabel('x(t)');
 
-Signal_Spectometer(dataLeft, freq)
+% Show signal
+figure(1);
+plot(data_); title('Signal'); xlabel('Time (s)'); ylabel('Amplitude');
+
+% Present on image
+figure(2);
+ffts = Signal_Spectometer(data_, freq);
+imagesc(abs(ffts));
+title('Amplitude Spectrum of signal over time');
+xlabel('Time (s)'); ylabel('Frequency (Hz)');
