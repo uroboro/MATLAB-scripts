2122f7bd462d3782148e3682418018f448d9e8ce
uroboro <uroboro845@gmail.com>
2015-08-19 23:13:09 -0300

    [DSP] Fixed fsenoidal usage of linspace

--- dsp/fsenoidal.m
+++ dsp/fsenoidal.m
@@ -1,8 +1,6 @@
 %fsenoidal Funcion senoidal
 %   Detailed explanation goes here
 function [ y, t ] = fsenoidal( A, f0, F, offset, N, fs )
-%    t = 0:1/fs:(N-1)/fs;
-    t = linspace(0, (N - 1) / fs, fs);
-    t = t.';
+    t = transpose(linspace(0, (N - 1) / fs, N));
     y = offset + A * sin(2 * pi * f0 * t + F);
 end
