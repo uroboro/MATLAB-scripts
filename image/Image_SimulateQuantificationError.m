af9451519eb529f2ba14b8880b520b80ba8fef75
uroboro <uroboro845@gmail.com>
2015-08-22 19:20:33 -0300

    [Image] Added quantification error simulator

---
+++ image/Image_SimulateQuantificationError.m
@@ -0,0 +1,20 @@
+%IMAGE_SIMULATEQUANTIFICATIONERROR Simulates quantification error on an image
+% Usage:
+%       image = Image_SimulateQuantificationError(imgSource, colorDepth);
+function [ output_args ] = Image_SimulateQuantificationError( image, colorDepth )
+	tmp = uint16(image);
+	[rows, cols] = size(image);
+
+	if (colorDepth > 8)
+		error('Color depth cannot be more than 8.');
+	end
+	
+	for x = 1:1:rows
+		for y = 1:1:cols
+			a = tmp(x, y);
+			tmp(x, y) = a-mod(a,2^(8-colorDepth))-1;
+		end
+	end
+
+	output_args = uint8(tmp);
+end
