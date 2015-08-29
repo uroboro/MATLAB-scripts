3d2aab41ce12066f3d93370448a67b1fe80442e3
uroboro <uroboro845@gmail.com>
2015-08-29 12:29:57 -0300

    [Image] Added accumulated histogram function

---
+++ image/imhistac.m
@@ -0,0 +1,12 @@
+%IMHISTAC calculates the accumulated histogram for the intensity image and displays a plot of the histogram.
+% Syntax
+%
+% [counts,binLocations] = imhistac(img)
+function [ ha, x ] = imhistac( img )
+	[h, x] = imhist(img);
+	ha = h;
+	for ii = 2:length(h)
+		ha(ii) = h(ii) + ha(ii - 1);
+	end
+end
+
