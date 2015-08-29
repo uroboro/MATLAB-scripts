62fac42be1dccc6b5c16c40534d8065242c71882
uroboro <uroboro845@gmail.com>
2015-08-29 12:35:25 -0300

    [DSP] Added Twiddle factors tests

--- dsp/testbench2.m
+++ dsp/testbench2.m
@@ -1,17 +1,13 @@
-function testbench2()
-%TESTBENCH Summary of this function goes here
+%TESTBENCH Twiddle factors tests
 %   Detailed explanation goes here
-    samples = 100;
-    fase = 0;
-    offset = 0;
-    fs = 100;
 
-    freq = 10; amp = 1;
-    [y, t] = fsenoidal(freq, amp, fase, offset, samples, fs);
-    plot(t, y);
-    hold on;
-
-    Y = myDFT(y);
-    stem(1:length(Y), abs(Y));
-
+function testbench2
+	N = 16;
+	fs = 2*pi;
+	n=pi/4;
+	figure(2);
+	x = Signal_GenerateSine(N, fs, 1, n, pi/2);
+	subplot(1, 2, 1); plot(x);
+	X = myDFT(x);
+	subplot(1, 2, 2); stem(abs(X));
 end
