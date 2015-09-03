46c646d452f0fa551b126b7e27e3e7a60485b1b0
uroboro <uroboro845@gmail.com>
2015-09-02 23:13:11 -0300

    [DSP/myDFT] Added frequencies output

--- dsp/myDFT.m
+++ dsp/myDFT.m
@@ -1,11 +1,14 @@
-function [ X ] = myDFT( x )
+function [ X, F ] = myDFT( x )
 %MYDFT Summary of this function goes here
 %   Detailed explanation goes here
     N = length(x);
     n = 0:N-1;
     X_ = zeros(1,N);
+    F_ = zeros(1,N);
     for ii = 0:N-1
         X_(ii+1) = x.' * exp(1i * 2 * pi * n.' / N * ii);
+        F_(ii+1) = ii;
     end
     X = X_.';
+	F = F_.';
 end
