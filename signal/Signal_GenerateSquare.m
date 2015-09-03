f1fe094fcac19958f857b52be9bb9056f032ca2f
uroboro <uroboro845@gmail.com>
2015-09-02 23:53:15 -0300

    [Signal/Square] First working implementation
    * Needs to incorporate phase into output signal.

--- signal/Signal_GenerateSquare.m
+++ signal/Signal_GenerateSquare.m
@@ -2,18 +2,20 @@
 %       image = Signal_GenerateSquare(samples, samplingFrequency, amplitude, frequency);
 %       image = Signal_GenerateSquare(samples, samplingFrequency, amplitude, frequency, percentageOn);
 %       image = Signal_GenerateSquare(samples, samplingFrequency, amplitude, frequency, percentageOn, phase);
-%       image = Signal_GenerateSquare(samples, samplingFrequency, amplitude, frequency, percentageOn, phase, offset);
 function [ out, time ] = Signal_GenerateSquare( samples, samplingFrequency, amplitude, frequency, varargin )
 	defaults = [0.5, 0, 0];
 	for i = 1:length(varargin)
 		% copio los argumentos que existen.
 		defaults(i) = varargin{i};
 	end
-	percentageOn = defaults(1);
+	percentage = defaults(1);
 	phase = defaults(2);
-	offset = defaults(3);
 
 	time = Base_GenerateSamples(samples, samplingFrequency);
-%     cycle = length(time)/frequency
-    out = offset + amplitude * sin(2 * pi * frequency * time + phase);
+    cycle = 1/frequency;
+	tmp = mod(time, cycle);
+	for i = 1:length(time);
+		tmp(i) = (tmp(i) < percentage * cycle);
+	end
+    out = amplitude * tmp;
 end
