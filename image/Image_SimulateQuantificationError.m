af1c53f9b13ef42e3c5a77fce56c05e84afc5256
uroboro <uroboro845@gmail.com>
2015-09-09 02:31:52 -0300

    [Image/Quantification] Fixed equation

--- image/Image_SimulateQuantificationError.m
+++ image/Image_SimulateQuantificationError.m
@@ -2,7 +2,7 @@
 % Usage:
 %       image = Image_SimulateQuantificationError(imgSource, colorDepth);
 function [ output_args ] = Image_SimulateQuantificationError( image, colorDepth )
-	tmp = uint16(image);
+	tmp = double(image);
 	[rows, cols] = size(image);
 
 	if (colorDepth > 8)
@@ -12,7 +12,9 @@ function [ output_args ] = Image_SimulateQuantificationError( image, colorDepth 
 	for x = 1:1:rows
 		for y = 1:1:cols
 			a = tmp(x, y);
-			tmp(x, y) = a - mod(a, 2^(8-colorDepth)) - 1;
+			% tmp(x, y) = a - mod(a, 2^(8-colorDepth)) - 1;
+			tmp(x, y) = 2^8 / (2^colorDepth - 1) * floor(a * 2^(colorDepth - 8));
+			% tmp(x, y) = a * (2^colorDepth + 1) / 2^(8 - colorDepth);
 		end
 	end
 
