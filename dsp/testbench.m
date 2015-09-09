5d17895195629ffa15be7ece87ce717c301c6071
uroboro <uroboro845@gmail.com>
2015-09-09 02:37:46 -0300

    [DSP/testbench] Optimization test on FFT vs myDFT

--- dsp/testbench.m
+++ dsp/testbench.m
@@ -1,17 +1,28 @@
-function testbench()
 %TESTBENCH Summary of this function goes here
 %   Detailed explanation goes here
-    samples = 100;
-    fase = 0;
-    offset = 0;
-    fs = 100;
+function testbench()
+	fase = 0;
+	offset = 0;
+	fs = 200;
 
-    freq = 10; amp = 1;
-    [y, t] = fsenoidal(freq, amp, fase, offset, samples, fs);
-    plot(t, y);
-    hold on;
+	freq = 10; amp = 1;
 
-    Y = myDFT(y);
-    stem(1:length(Y), abs(Y));
-
+	tests = 3;
+	N = logspace(1, tests);
+	M = length(N);
+	timeFFT = zeros(M, 1);
+	timeDFT = zeros(M, 1);
+	for i = 1:M
+		[y, t] = fsenoidal(amp, freq, fase, offset, N(i), fs);
+% 		plot(y);
+% 		hold on;
+		tic;
+		Y = fft(y);
+		timeFFT(i) = toc;
+		tic;
+		[Y, F] = myDFT(y, 1);
+		timeDFT(i) = toc;
+% 		stem(F, abs(Y));
+	end
+	plot(N, timeFFT, N, timeDFT); title('Comparacion de tiempos'); xlabel('Cantidad de muestras'); ylabel('Tiempo [s]'); legend('FFT', 'myDFT');
 end
