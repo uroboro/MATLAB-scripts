e57631d6d6214a235246f994ceca13e1e9ac0272
uroboro <uroboro845@gmail.com>
2015-09-09 18:12:49 -0300

    [Image/imhistac] Fix help text

--- image/imhistac.m
+++ image/imhistac.m
@@ -1,7 +1,6 @@
 %IMHISTAC calculates the accumulated histogram for the intensity image and displays a plot of the histogram.
-% Syntax
-%
-% [counts,binLocations] = imhistac(img)
+% Syntax
+%	[counts,binLocations] = imhistac(img)
 function [ ha, x ] = imhistac( img )
 	[h, x] = imhist(img);
 	ha = h;
