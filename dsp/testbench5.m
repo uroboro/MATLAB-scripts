12db6f85499655dc943feaf411edb15b72084184
uroboro <uroboro845@gmail.com>
2015-10-03 18:52:04 -0300

    [DSP] Periodogram improvements

--- dsp/testbench5.m
+++ dsp/testbench5.m
@@ -5,24 +5,43 @@ function [ output_args ] = testbench5( varargin )
 		% copio los argumentos que existen.
 		defaults(i) = varargin{i};
 	end
-	var = defaults(1);
+	varianza = defaults(1);
 	tic;
 
-	R = 100; % Realizaciones
-	N = [10e2,10e3,10e4,10e5]; % Muestras
+	R = 1e3; % Realizaciones
+	N = [2^10, 2^11, 2^12, 2^13]; % Muestras
 
-	for n = 1:length(N)
-		x = sqrt(var) * randn(N(n), R);
-		%plot(x);
-		X = 1 / N(n) * (abs(fft(x))).^2 - var;
-		subplot(211);
-		plot(X(:,1)); title('Periodogram');
+	output_args = [];
+	
+	rows = 4;
+	for i = 1:length(N)
+		x = sqrt(varianza) * randn(N(i), R);
 
-		X_ = mean(X, 1);
-		subplot(212);
-		plot(X_); title({'Periodogram para ' num2str(R) ' repeticiones'});
+		figure(i);
+		X_p = periodogram(x);
+		subplot(rows,1,1);
+		plot(X_p); title('Periodogram');
+		
+		for ii=1:R
+			ffft = fft(x(:,ii));
+			ffft2 = ffft(1:length(ffft)/2+1);
+			clear ffft;
+			X(:,ii) = 1 / N(i) * (abs(ffft2)).^2;
+			clear ffft2;
+		end
+		
+		subplot(rows,1,2);
+ 		plot(X); title('Periodograma');
 
-		output_args(n) = mean(X_);
+		X_ = mean(X, 2);
+		subplot(rows,1,3);
+		plot(X_); title('mean Periodogram');
+		
+		Y_ = var(X, 0, 2);
+		subplot(rows,1,4);
+		plot(Y_); title('var Periodogram');
+
+		output_args = [ output_args; X_(1), Y_(1)];
 	end
 
 	toc
