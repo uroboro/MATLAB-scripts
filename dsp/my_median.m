a34df3a0c382229c45c5f88fdee19ce6f61d81dd
uroboro <uroboro845@gmail.com>
2015-10-07 16:33:30 -0300

    [DSP] Added median calculating function

---
+++ dsp/my_median.m
@@ -0,0 +1,19 @@
+% Calculate median of signal
+function [ output_args ] = my_median( x, L )
+	N = length(x);
+	X = x;
+	for n = 1:N
+		x_ = [];
+		for i = -floor(L/2) : floor(L/2)
+			n_ = n + i;
+			if (n_ < 1)
+				n_ = N + n_;
+			elseif (n_ > N)
+				n_ = n_ - N;
+			end
+			x_ = [x_; x(n_)];
+		end
+		X(n) = median(x_);
+	end
+	output_args = X;
+end
